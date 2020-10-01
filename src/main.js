16c8f40a4ddcb442de8788073715e9e04308c461
manhcuongdtbk <manhcuongdtbk@gmail.com>
2020-10-01 19:03:35 +0700

    setup firebase

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Vue from 'vue';
+import { firestorePlugin } from 'vuefire';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-// Install BootstrapVue
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false;
 
